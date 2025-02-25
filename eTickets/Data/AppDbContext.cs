166b1591a280c9ca091129332c4baf3e99b9605b
Asus <mehrbodmollakazemi81@gmail.com>
2025-02-25 22:28:40 +0330

    Added DbContext dependency.

---
+++ eTickets/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data
+{
+    public class AppDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
