0e24940224a72361cad905d3f064250082526ba5
Asus <mehrbodmollakazemi81@gmail.com>
2025-02-26 00:19:07 +0330

    Designed AppDbContext.

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Entity;
+using eTickets.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace eTickets.Data
@@ -9,5 +10,24 @@ namespace eTickets.Data
         {
             
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+            });
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.ActorId);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public Microsoft.EntityFrameworkCore.DbSet<Actor> Actors { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Cinema> Cinemas { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Movie> Movies { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Producer> Producers { get; set; }
+
+        public Microsoft.EntityFrameworkCore.DbSet<Actor_Movie> Actors_Movies { get; set; }
     }
 }
--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace eTickets.Models
 {
@@ -12,5 +13,8 @@ namespace eTickets.Models
         public string Name { get; set; }
 
         public string Descrition { get; set; }
+
+        // Relationships.
+        public List<Movie> Movies { get; set; }
     }
 }
